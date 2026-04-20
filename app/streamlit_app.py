e791b6a48660467886aea17857b52468da6f7153
hanneskolk <hanneskolk@gmail.com>
2026-04-20 15:36:29 +0300

    possible fix

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -17,14 +17,5 @@ if file:
         st.error(res.text)
     else:
         st.write("Looking for response")
-        try:
-            data = res.json()
-            st.write(data)
-        except Exception as e:
-            st.error("Response is not JSON")
-            st.write(res.text[:1000])
-        st.write(res.headers)
-        st.write(res.text)
-        st.write(data["output"])
         st.success("Done")
         st.video(res.content)
